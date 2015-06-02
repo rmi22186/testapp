f18b1ae041961d852cb72a28615b532a6802745e
Robert Ing <rmi22186@gmail.com>
2015-06-01 22:43:57 -0400

    make website continue to ping self to stay open on heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var http = require("http");
 
 app.use(express.static(__dirname));
 module.exports = app;
@@ -7,3 +8,8 @@ module.exports = app;
 var server = app.listen(process.env.PORT || 3000, function() {
   console.log('express server listening on port ' + server.address().port);
 });
+
+
+setInterval(function() {
+    http.get("http://cherrystreet.herokuapp.com");
+}, 5000); 
