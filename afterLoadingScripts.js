1529528fba23b26bda465a3117260d5ce8740f96
Robert Ing <ring@mparticle.com>
2017-03-22 15:45:01 -0400

    test app set up!

--- afterLoadingScripts.js
+++ afterLoadingScripts.js
@@ -1,3 +1,5 @@
+/* eslint-disable*/
+// TEST APP
 mParticle.configureForwarder({
         "name":"Amplitude",
         "moduleId":53,
@@ -9,7 +11,7 @@ mParticle.configureForwarder({
             "apiKey":"04c5e771c7f0222f886df7ef18c82998",
             "includeEmailAsUserProperty":"True",
             "userIdentification":"customerId",
-            "instanceName": "default"
+            "instanceName": "instance1"
         },
         "screenNameFilters":[],
         "screenAttributeFilters":[],
@@ -19,6 +21,35 @@ mParticle.configureForwarder({
         "eventTypeFilters":[],
         "attributeFilters":[],
         "githubPath":null,
-        "filteringEventAttributeValue":null});
+        "filteringEventAttributeValue":null
+});
+
+mParticle.configureForwarder({"name":"MixpanelEventForwarder","moduleId":10,"isDebug":true,"isVisible":true,"isDebugString":"true","hasDebugString":"false","settings":{"token":"c2769f762f35f53758c27e4e421c05da","useMixpanelPeople":"True","includeUserAttributes":"True","includeAttributionInfo":"False","useCustomerId":"True","forwardSessionStartEnd":"True","createProfileOnlyIfLoggedIn":"False","includeIpAddress":"True","sessionEndEventName":"session-end","sessionStartEventName":"session-start"},"screenNameFilters":[],"screenAttributeFilters":[],"userIdentityFilters":"","userAttributeFilters":[],"eventNameFilters":[],"eventTypeFilters":[],"attributeFilters":[],"githubPath":null,"filteringEventAttributeValue":null});
+
+
+// //Another Test App
+mParticle.configureForwarder({
+        "name":"Amplitude",
+        "moduleId":53,
+        "isDebug":true,
+        "isVisible":true,
+        "isDebugString":"true",
+        "hasDebugString":"false",
+        "settings":{
+            "apiKey":"1d9ab59e57cb183da22710de5eb970fa",
+            "includeEmailAsUserProperty":"True",
+            "userIdentification":"customerId",
+            "instanceName": "instance2"
+        },
+        "screenNameFilters":[],
+        "screenAttributeFilters":[],
+        "userIdentityFilters":"",
+        "userAttributeFilters":[],
+        "eventNameFilters":[],
+        "eventTypeFilters":[],
+        "attributeFilters":[],
+        "githubPath":null,
+        "filteringEventAttributeValue":null
+});
 
 mParticle.init('e9ea8a9ba0c5bd48828e4cac23be681b')
--- server.js
+++ server.js
@@ -1,18 +1,5 @@
 var express = require('express');
 var app = express();
-var http = require("http");
-var username = 'e001b2b2eb5da649a42c633563135de1';
-var password = 'Kfjqo310EvDZxAZYAeMB6V5_ldXTOWKGBNkwLL-QgG-MOWha5Zw7Ss5G71WbNQDl';
-var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
-
-var options = {
-    method:"POST",
-    host: 'https://api.mparticle.com/',
-    // path: 'oauth2/token',
-    headers:{
-        "Authorization": auth,
-    }
-};
 
 app.use(express.static(__dirname));
 module.exports = app;
@@ -20,19 +7,3 @@ var server = app.listen(8080, function() {
   console.log('express server listening on port ' + server.address().port);
 
 });
-
-// var request = http.request(options, function (response, err) {
-//     console.log('err', err);
-//     console.log('STATUS: ' + response.statusCode);
-//     console.log('HEADERS: ' + JSON.stringify(response.headers));
-//     response.setEncoding('utf8');
-//     response.on('data', function (chunk) {
-//         console.log('BODY: ' + chunk);
-//     });
-// });
-// request.end();
-
-
-setInterval(function() {
-    http.get("http://cherrystreet.herokuapp.com");
-}, 300000);
