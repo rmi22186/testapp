d73f2168eb28a9b4d487d0f1b06daaf191d3e264
Robert Ing <ring@mparticle.com>
2017-07-09 22:13:56 -0400

    Sync

--- afterLoadingScripts.js
+++ afterLoadingScripts.js
@@ -1,34 +1,329 @@
-/* eslint-disable*/
-// TEST APP
+// /* eslint-disable*/
+// // TEST APP
+//
+// window.mParticle.configureForwarder({
+//     name:'Amplitude',
+//     moduleId:53,
+//     isDebug:true,
+//     isVisible:true,
+//     isDebugString:'true',
+//     hasDebugString:'false',
+//     settings:{
+//         apiKey:'375161e7789341194e6ba46b523ff3b4',
+//         userIdentification:'useIdentity',
+//         includeEmailAsUserProperty:'False',
+//         instanceName:'default'
+//     },
+//     screenNameFilters:[],
+//     screenAttributeFilters:[],
+//     userIdentityFilters:'AQU=',
+//     userAttributeFilters:[],
+//     eventNameFilters:[],
+//     eventTypeFilters:[],
+//     attributeFilters:[],
+//     githubPath:null,
+//     filteringEventAttributeValue:null
+// });
+// window.mParticle.configureForwarder({
+//     "name":"Amplitude",
+//     "moduleId":53,
+//     "isDebug":false,
+//     "isVisible":true,
+//     "isDebugString":"false",
+//     "hasDebugString":"true",
+//     "settings":{"apiKey":"375161e7789341194e6ba46b523ff3b4",
+//     "userIdentification":"useIdentity",
+//     "includeEmailAsUserProperty":"False",
+//     "instanceName":"default"},
+//     "screenNameFilters":[],
+//     "screenAttributeFilters":[],
+//     "userIdentityFilters":"AQU=",
+//     "userAttributeFilters":[],
+//     "eventNameFilters":[],
+//     "eventTypeFilters":[],
+//     "attributeFilters":[],
+//     "githubPath":null,
+//     "filteringEventAttributeValue":null});
+//
+// window.mParticle.configureForwarder({
+//     "name":"Appboy",
+//     "moduleId":28,
+//     "isDebug":true,
+//     "isVisible":true,
+//     "isDebugString":"true",
+//     "hasDebugString":"false",
+//     "settings":{"apiKey":"6eac4723-c0a8-4a9d-9029-eea429090df9",
+//     "push_enabled":"True",
+//     "register_inapp":"True",
+//     "appGroupId":"",
+//     "addEventAttributeList":"[]",
+//     "removeEventAttributeList":"[]",
+//     "singleItemEventAttributeList":"[]",
+//     "abkFlushIntervalOptionKey":null,
+//     "abkRequestProcessingPolicyOptionKey":null,
+//     "abkSessionTimeoutKey":null,
+//     "abkMinimumTriggerTimeIntervalKey":null,
+//     "abkCollectIDFA":"False"},
+//     "screenNameFilters":[],
+//     "screenAttributeFilters":[-1350758925],
+//     "userIdentityFilters":"",
+//     "userAttributeFilters":[],
+//     "eventNameFilters":[],
+//     "eventTypeFilters":[1568,1576],
+//     "attributeFilters":[-107771678],
+//     "githubPath":null,
+//     "filteringEventAttributeValue":null});
+//
+// window.mParticle.configureForwarder({
+//     "name":"Appboy",
+//     "moduleId":28,
+//     "isDebug":false,
+//     "isVisible":true,
+//     "isDebugString":"false",
+//     "hasDebugString":"true",
+//     "settings":{"apiKey":"6eac4723-c0a8-4a9d-9029-eea429090df9",
+//     "push_enabled":"True",
+//     "register_inapp":"True",
+//     "appGroupId":"",
+//     "addEventAttributeList":"[]",
+//     "removeEventAttributeList":"[]",
+//     "singleItemEventAttributeList":"[]",
+//     "abkFlushIntervalOptionKey":null,
+//     "abkRequestProcessingPolicyOptionKey":null,
+//     "abkSessionTimeoutKey":null,
+//     "abkMinimumTriggerTimeIntervalKey":null,
+//     "abkCollectIDFA":"False"},
+//     "screenNameFilters":[],
+//     "screenAttributeFilters":[-1350758925],
+//     "userIdentityFilters":"",
+//     "userAttributeFilters":[],
+//     "eventNameFilters":[],
+//     "eventTypeFilters":[1568, 1576],
+//     "attributeFilters":[-107771678],
+//     "githubPath":null,
+//     "filteringEventAttributeValue":null});
+//
+// window.mParticle.configureForwarder({"name":"LeanplumAnalyticsEventForwarder",
+//     "moduleId":28,
+//     "isDebug":true,
+//     "isVisible":true,
+//     "isDebugString":"false",
+//     "hasDebugString":"true",
+//     "userIdentityFilters": "",
+//     "settings":{
+//         "apiKey":"dev_Bd2Tu0iDcQlfE7RgpGuSUki3YZeeX1scaPEV1BukFn0",
+//         "appId":"app_SXoOYT2li2hXX47ZokeOp0I1aSJZKHnnru11Dd3N7O0"
+//     },
+//     "screenNameFilters":[],
+//     "screenAttributeFilters":[-1350758925],
+//     "userIdentityFilters":"",
+//     "userAttributeFilters":[],
+//     "eventNameFilters":[],
+//     "eventTypeFilters":[1568,1576],
+//     "attributeFilters":[-107771678],
+//     "githubPath":null,
+//     "filteringEventAttributeValue":null});
+//
+// window.mParticle.configureForwarder({"name":"LeanplumAnalyticsEventForwarder",
+//     "appId":"app_SXoOYT2li2hXX47ZokeOp0I1aSJZKHnnru11Dd3N7O0",
+//     "moduleId":28,
+//     "isDebug":true,
+//     "isVisible":true,
+//     "isDebugString":"false",
+//     "hasDebugString":"true",
+//     "settings":{
+//          "apiKey":"prod_ANZ3ACHgDNhwM82y6MjAHIF3twGDhDNlDf7MSSbX1Io",
+//          "appId":"app_SXoOYT2li2hXX47ZokeOp0I1aSJZKHnnru11Dd3N7O0"
+//     }
+// });
+//
+// window.mParticle.configureForwarder({
+//     "name":"GoogleAnalyticsEventForwarder",
+//     "moduleId":6,
+//     "isDebug":true,
+//     "isVisible":true,
+//     "isDebugString":"true",
+//     "hasDebugString":"false",
+//     "settings":{"apiKey":"UA-96876767-1",
+//     "customMetrics":"[]",
+//     "lateEventAction":"Send",
+//     "sendUserIPAddress":"True",
+//     "sendAdvertisingIds":"True",
+//     "defaultApplicationName":null,
+//     "classicMode":"False",
+//     "useLocalhostCookie":"False",
+//     "useCustomerId":"True",
+//     "customDimensions":"[]",
+//     "enableEnhancedEcommerce":"True"},
+//     "screenNameFilters":[],
+//     "screenAttributeFilters":[],
+//     "userIdentityFilters":"",
+//     "userAttributeFilters":[],
+//     "eventNameFilters":[-721183254,-1098223750,-307449490,218310125,522674465,-1500486713,-1765589058,-1471616309,-990739254,-1170357611,1147264951,-2123416925,282183495,284332181,-1633107774,921213094,-1183138556,1615888123,-1905777813,-558278820,1509379648,-585089059,-224537836,234876680,627244470,1629261791,-1980920479,1279587230,1139120425,1020413301,901333808,-196527491,-2026712319,1593206787,-281095028],"eventTypeFilters":[48,50,51,53,55,57,1567,1568,1569,1573,1574],"attributeFilters":[-1820956291,993389335,-773269666,372242825,-1390768208,1528499355,1034317854,2002009431,-118999439,-2023916437,2062655048,1283012054,618707982,-1324608919,-2133570213,-1034924723,1998977628,1799049285,-968618220,-1724720418,1706786091,2100958644,-208294769,962388044,-432964392,-1625390339,22426488,-836475596,2043539578,1220956386,694434596,-1099631484,1587021811,2100162863,1290501489,400523287,-1319564116,1989630418,-2048804700,-1133494303,-1659558490],
+//     "githubPath":null,
+//     "filteringEventAttributeValue":null});
+//
+// window.mParticle.configureForwarder({
+//     "name":"mparticledevicematch",
+//     "moduleId":109,
+//     "isDebug":true,
+//     "isVisible":true,
+//     "isDebugString":"true",
+//     "hasDebugString":"false",
+//     "settings":{"partnerId":"2254"},
+//     "screenNameFilters":[],
+//     "screenAttributeFilters":[],
+//     "userIdentityFilters":"",
+//     "userAttributeFilters":[],
+//     "eventNameFilters":[],
+//     "eventTypeFilters":[],
+//     "attributeFilters":[],
+//     "githubPath":null,
+//     "filteringEventAttributeValue":null});
+//
+// window.mParticle.configureForwarder({
+//     "name":"mparticledevicematch",
+//     "moduleId":109,
+//     "isDebug":false,
+//     "isVisible":true,
+//     "isDebugString":"false",
+//     "hasDebugString":"true",
+//     "settings":{"partnerId":"2254"},
+//     "screenNameFilters":[],
+//     "screenAttributeFilters":[],
+//     "userIdentityFilters":"",
+//     "userAttributeFilters":[],
+//     "eventNameFilters":[],
+//     "eventTypeFilters":[],
+//     "attributeFilters":[],
+//     "githubPath":null,
+//     "filteringEventAttributeValue":null});
+//
+// // FL Test App - prod
+// ;window.mParticle.configureForwarder({
+//     "name":"Amplitude",
+//     "testName": 'FL Test App Prod', "moduleId":53,
+//     "isDebug":false,
+//     "isVisible":true,
+//     "isDebugString":"false",
+//     "hasDebugString":"true",
+//     "settings":{"instanceName":"default",
+//     "includeEmailAsUserProperty":"False",
+//     "userIdentification":"customerId",
+//     "apiKey":"975543ccd1976e1aad486c5d47c14c70"},
+//     "screenNameFilters":[],
+//     "screenAttributeFilters":[],
+//     "userIdentityFilters":"AQU=",
+//     "userAttributeFilters":[],
+//     "eventNameFilters":[],
+//     "eventTypeFilters":[],
+//     "attributeFilters":[],
+//     "githubPath":null,
+//     "filteringEventAttributeValue":null});
+//
+// // Test App - both
+// ;window.mParticle.configureForwarder({
+//     "name":"Amplitude",
+//     "testName": 'test app dev',
+//     "moduleId":53,
+//     "isDebug":true,
+//     "isVisible":true,
+//     "isDebugString":"true",
+//     "hasDebugString":"false",
+//     "settings":{"apiKey":"04c5e771c7f0222f886df7ef18c82998",
+//     "userIdentification":"customerId",
+//     "includeEmailAsUserProperty":"False",
+//     "instanceName":"testApp"},
+//     "screenNameFilters":[],
+//     "screenAttributeFilters":[],
+//     "userIdentityFilters":"AQU=",
+//     "userAttributeFilters":[],
+//     "eventNameFilters":[],
+//     "eventTypeFilters":[],
+//     "attributeFilters":[],
+//     "githubPath":null,
+//     "filteringEventAttributeValue":null});
+//
+// // Test App - both
+// ;window.mParticle.configureForwarder({
+//     "name":"Amplitude",
+//     "testName": 'test app - prod',
+//     "moduleId":53,
+//     "isDebug":false,
+//     "isVisible":true,
+//     "isDebugString":"false",
+//     "hasDebugString":"true",
+//     "settings":{"apiKey":"04c5e771c7f0222f886df7ef18c82998",
+//     "userIdentification":"customerId",
+//     "includeEmailAsUserProperty":"False",
+//     "instanceName":"testApp"},
+//     "screenNameFilters":[],
+//     "screenAttributeFilters":[],
+//     "userIdentityFilters":"AQU=",
+//     "userAttributeFilters":[],
+//     "eventNameFilters":[],
+//     "eventTypeFilters":[],
+//     "attributeFilters":[],
+//     "githubPath":null,
+//     "filteringEventAttributeValue":null});
+//
+// // Another Test app - dev
+// ;window.mParticle.configureForwarder({
+//     "name":"Amplitude",
+//     "testName": 'another test app - dev',
+//     "moduleId":53,
+//     "isDebug":true,
+//     "isVisible":true,
+//     "isDebugString":"true",
+//     "hasDebugString":"false",
+//     "settings":{"instanceName":"anotherTestApp gogo",
+//     "includeEmailAsUserProperty":"False",
+//     "userIdentification":"customerId",
+//     "apiKey":"1d9ab59e57cb183da22710de5eb970fa"},
+//     "screenNameFilters":[],
+//     "screenAttributeFilters":[],
+//     "userIdentityFilters":"AQU=",
+//     "userAttributeFilters":[],
+//     "eventNameFilters":[],
+//     "eventTypeFilters":[],
+//     "attributeFilters":[],
+//     "githubPath":null,
+//     "filteringEventAttributeValue":null});
+//
+// window.mParticle.configurePixel({
+//     "name":"AdobeEventForwarder",
+//     "moduleId":11,
+//     "esId":24053,
+//     "isDebug":true,
+//     "isProduction":true,
+//     "settings":{},
+//     "frequencyCap":1,
+//     "pixelUrl":"https://dpm.demdex.net/ibs:dpid=123123&dpuuid=%%mpid%%&redir=",
+//     "redirectUrl":"http://api-qa.mparticle.com/v1/sync?esid=24053&MPID=%%mpid%%&ID=${DD_UUID}&Key=174ece0c0838c344937d28dd0a91de6b"
+// });
+//
+// window.mParticle.configurePixel({
+//     "name":"AdobeEventForwarder",
+//     "moduleId":11,
+//     "esId":24293,
+//     "isDebug":true,
+//     "isProduction":true,
+//     "settings":{},
+//     "frequencyCap":7,
+//     "pixelUrl":"https://dpm.demdex.net/ibs:dpid=130884&amp;dpuuid=%%mpid%%&amp;redir=",
+//     "redirectUrl":"https://api-qa.mparticle.com/v1/sync?esid=24293&amp;MPID=%%mpid%%&amp;ID=${DD_UUID}&amp;Key=e001b2b2eb5da649a42c633563135de1"
+// });
+var options = {
+    apiKey: 'e001b2b2eb5da649a42c633563135de1',
+    initialIdentity: {
+        userIdentities: {
+            5: 'test123',
+            6: 'foo@bar.com'
+        },
+        copyUserAttributes: true
+    }
+};
 
-window.mParticle.configureForwarder({"name":"Appboy",
-    "moduleId":28,
-    "isDebug":true,
-    "isVisible":true,
-    "isDebugString":"true",
-    "hasDebugString":"false",
-    "settings":{
-        "apiKey":"9c498bac-925b-4f98-b5ca-165dc1ac87e7",
-        "push_enabled":"True",
-        "register_inapp":"True",
-        "appGroupId":"",
-        "singleItemEventAttributeList":"",
-        "removeEventAttributeList":"",
-        "addEventAttributeList":"",
-        "abkFlushIntervalOptionKey":null,
-        "abkRequestProcessingPolicyOptionKey":null,
-        "abkSessionTimeoutKey":null,
-        "abkMinimumTriggerTimeIntervalKey":null,
-        "abkCollectIDFA":"False"},
-        "screenNameFilters":[],
-        "screenAttributeFilters":[],
-        "userIdentityFilters":"",
-        "userAttributeFilters":[],
-        "eventNameFilters":[],
-        "eventTypeFilters":[],
-        "attributeFilters":[],
-        "githubPath":null,
-        "filteringEventAttributeValue":null
-    });
-
-mParticle.init('e9ea8a9ba0c5bd48828e4cac23be681b')
+mParticle.init(options); // prod
+// mParticle.init('beab3f4d34281d45bfcdbbd7eb21c083'); // prod
+// mParticle.init('e001b2b2eb5da649a42c633563135de1'); // qa
--- gulpfile.js
+++ gulpfile.js
@@ -2,45 +2,13 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var nodemon = require('gulp-nodemon');
-var sass = require('gulp-sass');
-var bower = require('gulp-bower');
-var rename = require('gulp-rename');
-var minifyCss = require('gulp-minify-css');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-
-
-var config = {
-  js: 'dev/js/*.js',
-  sassPath: './sass',
-  bowerDir: './bower_components',
-  // fontawesome: config.bowerDir + 'fontawesome'
-};
-
-// gulp.task('bower', function() {
-//   return bower()
-//     .pipe(gulp.dest(config.bowerDir));
-// });
-
-// gulp.task('icons', function() {
-//   return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*)'
-//     .pipe(gulp.dest('./public/fonts'))
-//   );
-// });
 
 gulp.task('browser-sync', ['nodemon'], function() {
   browserSync({
-    proxy: "localhost:3000",  // local node app address
+    proxy: "localhost:8080",  // local node app address
     port: 5000,  // use *different* port than above
     notify: true
   });
-});
-
-gulp.task('uglifyjs', function() {
-  gulp.src(config.js)
-  .pipe(uglify())
-  .pipe(rename('init.min.js'))
-  .pipe(gulp.dest('public/js/'));
 });
 
 gulp.task('nodemon', function (cb) {
@@ -65,23 +33,6 @@ gulp.task('nodemon', function (cb) {
   });
 });
 
-gulp.task('compile-sass', function () {
-  gulp.src('./dev/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dev/css'));
-});
-
-gulp.task('concat-css', ['compile-sass'], function () {
-  gulp.src('./dev/css/**/*.css')
-    .pipe(concat('styles.min.css'))
-    .pipe(minifyCss())
-    .pipe(gulp.dest('public/css'));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./dev/scss/**/*.scss', ['concat-css'], reload);
-});
-
-gulp.task('default', ['browser-sync','uglifyjs','concat-css', 'sass:watch'], function () {
+gulp.task('default', ['browser-sync',], function () {
   gulp.watch(['index.html', 'public/css/style.min.css'], reload);
 });
